8f7c326419b2d01c8c102669bfe59cafd9452fae
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-23 13:16:20 -0400

    Hover Feature done

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,9 @@ function createGrid(size) {
     for (let j = 0; j < n; j++) {
       const div = document.createElement('div');
       applyDivStyleGeneral(div);
+      div.classList.add('gridItem');
+      div.addEventListener('mouseover', mouseHover);
+      div.addEventListener('mouseout', mouseOff);
       row.appendChild(div);
     }
     container.appendChild(row);
@@ -27,4 +30,23 @@ function applyDivStyleGeneral(div) {
     width: 50px;
     height: 50px;`
   );
+}
+
+function applyDivStyleHover(div) {
+  div.setAttribute(
+    'style',
+    `border-style: solid;
+    border-color: black;
+    background-color: blue;
+    width: 50px;
+    height: 50px;`
+  );
+}
+
+function mouseHover(e) {
+  applyDivStyleHover(e.target);
+}
+
+function mouseOff(e) {
+  applyDivStyleGeneral(e.target);
 }
