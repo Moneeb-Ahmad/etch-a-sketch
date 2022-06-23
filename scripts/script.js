12fef3cf481ea46c507404bd97ff30db1e55011c
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-23 14:47:27 -0400

    Button grid size selector done

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,13 @@
 const container = document.querySelector('.container');
-console.log(createGrid(8));
+const sizeSelector = document.querySelector('.gridChooser');
+let width = container.offsetWidth
+let n = 16;
+
+createGrid(n);
 
 function createGrid(size) {
-  let n = size;
+  removeAllChildrenFromContainer();
+  n = size;
   for (let i = 0; i < n; i++) {
     const row = document.createElement('div');
     row.setAttribute(
@@ -11,35 +16,68 @@ function createGrid(size) {
     );
     for (let j = 0; j < n; j++) {
       const div = document.createElement('div');
-      applyDivStyleGeneral(div);
+      applyDivStyleGeneral(div, n);
       div.classList.add('gridItem');
       div.addEventListener('mouseover', mouseHover);
       div.addEventListener('mouseout', mouseOff);
       row.appendChild(div);
+      applyRowStyle(row);
     }
     container.appendChild(row);
   }
 }
 
+function removeAllChildrenFromContainer() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
 function applyDivStyleGeneral(div) {
+  let height = Math.floor(width / n)
   div.setAttribute(
     'style',
-    `border-style: solid;
+    `display: flex;
+    border-width: 1px;
+    border-style: solid;
     border-color: black;
     background-color: pink;
-    width: 50px;
-    height: 50px;`
+    box-sizing: border-box;
+    width: ${width}px;
+    height: ${height}px;`
   );
 }
 
 function applyDivStyleHover(div) {
+  let height = Math.floor(width / n)
   div.setAttribute(
     'style',
-    `border-style: solid;
+    `display: flex;
+    border-width: 1px;
+    border-style: solid;
     border-color: black;
+    box-sizing: border-box;
     background-color: blue;
-    width: 50px;
-    height: 50px;`
+    width: ${width}px;
+    height: ${height}px;`
+  );
+}
+
+function applyRowStyle(row) {
+  let height = new Intl.NumberFormat('default', {
+    style: 'percent',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format((100 / n) / 100);
+
+  row.setAttribute(
+    'style',
+    `display:flex;
+    min-width: 100%;
+    max-width: 100%;
+    box-sizing: border-box;
+    min-height: ${height};
+    max-height: ${height};`
   );
 }
 
@@ -50,3 +88,18 @@ function mouseHover(e) {
 function mouseOff(e) {
   applyDivStyleGeneral(e.target);
 }
+
+function changeSize(e) {
+  let size = Number(prompt("Enter a grid size"));
+  if (Number.isInteger(size)) {
+    if (size > 0) {
+      if (size > 100) {
+        createGrid(100);
+      }
+      createGrid(size);
+    }
+  }
+}
+
+
+sizeSelector.addEventListener('click', changeSize);
